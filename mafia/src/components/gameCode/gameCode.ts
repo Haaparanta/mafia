84d08850920f8a8153c80533dda166369ccdfbd0
vesa haaparanta <vesahaaparanta@hotmail.fi>
2022-06-27 20:16:54 +0300

    code

---
+++ mafia/src/components/gameCode/gameCode.ts
@@ -0,0 +1,37 @@
+
+const validCodeLength = (code: string) => {
+  return code.length === 6;
+}
+
+const validCodeCharacters = (code: string) => {
+  return /^[0-9a-zA-Z]+$/.test(code);
+}
+
+
+// needs to connect to firebase
+const isNotUsed = (code: string) => {
+  return true;
+}
+
+export const generateGameCode = () => {
+  let code = Math.random().toString(36).substring(2, 8);
+  code = code.toUpperCase();
+  if (validCode(code)) {
+    return code;
+  }
+  code = generateGameCode();
+  return code;
+}
+
+
+const validCode = (code: string) => {
+  if (validCodeLength(code)) {
+    if (validCodeCharacters(code)) {
+      if (isNotUsed(code)) {
+        return true;
+      }
+    } 
+  }
+  return false;
+}
+
--- mafia/src/screens/menu/createGamePage.tsx
+++ mafia/src/screens/menu/createGamePage.tsx
@@ -1,8 +1,11 @@
-import React from "react"
+import React, { useState } from "react"
+import { generateGameCode } from "../../components/gameCode/gameCode"
 
 export const CreateGamePage = () => {
+  let gameCode = (generateGameCode())
   return (
     <div className='app'>
+      <h1>{gameCode}</h1>
       <h1>Create</h1>
     </div>
   )
