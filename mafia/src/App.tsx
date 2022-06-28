18e43c138c196ce463c85ad58415a35e8e548e41
vesa haaparanta <vesahaaparanta@hotmail.fi>
2022-06-28 22:38:16 +0300

    menupage with react routing

--- mafia/src/App.tsx
+++ mafia/src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Start } from './screens/start/start';
 import { Login } from './screens/login/login';
 import { MenuPage } from './screens/menu/menuPage';
-import { Start } from './screens/start/start';
-import { Routes, Route } from 'react-router-dom';
+import { CreateGamePage } from './screens/menu/createGamePage';
+import { JoinGamePage } from './screens/menu/joinGamePage';
+import { StorePage } from './screens/menu/storePage';
+import { StatisticsPage } from './screens/menu/statisticsPage';
+import { SettingsPage } from './screens/menu/settingsPage';
 
 function App() {
   return (
@@ -10,6 +15,11 @@ function App() {
       <Route path="/" element={<Start />} />
       <Route path="login" element={<Login />} />
       <Route path="menu" element={<MenuPage />} />
+      <Route path="create" element={<CreateGamePage />} />
+      <Route path="join" element={<JoinGamePage />} />
+      <Route path="store" element={<StorePage />} />
+      <Route path="statistics" element={<StatisticsPage />} />
+      <Route path="settings" element={<SettingsPage />} />
     </Routes>
   );
 }
--- mafia/src/screens/menu/menuPage.tsx
+++ mafia/src/screens/menu/menuPage.tsx
@@ -1,72 +1,57 @@
-import React, { useState } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 
-import { CreateGamePage } from "./createGamePage";
-import { JoinGamePage } from "./joinGamePage";
-import { StorePage } from "./storePage";
-import { StatisticsPage } from "./statisticsPage";
-import { SettingsPage } from "./settingsPage";
 import { signOutUser } from "../../components/firebase/logOut";
 
 import './menuPage.css';
 
 export const MenuPage = () => {
-  const [page, setPage] = useState("menu");
-  if (page === "menu") {
-    return (
-      <div className='app'>
-        <div className='container'>
-          <div className='item'>
-            <button onClick={() => setPage("create")}>Create Game</button>
-          </div>
-          <div className='item'>
-            <button onClick={() => setPage("join")}>Join Game</button>
-          </div>
-          <div className='item'>
-            <button onClick={() => setPage("store")}>Store</button>
-          </div>
-          <div className='item'>
-            <button onClick={() => setPage("statistics")}>Statistics</button>
-          </div>
-          <div className='item'>
-            <button onClick={() => setPage("settings")}>Settings</button>
-          </div>
-          <div className='item'>
-            <button onClick={signOutUser}>Sign Out</button>
-          </div>
+  return (
+    <div className='app'>
+      <div className='container'>
+        <div className='item'>
+          <Link to='/create'>
+            <button>
+              Create Game
+            </button>
+          </Link>
+        </div>
+        <div className='item'>
+          <Link to='/join'>
+            <button>
+              Join Game
+            </button>
+          </Link>
+        </div>
+        <div className='item'>
+          <Link to='/store'>
+            <button>
+              Store
+            </button>
+          </Link>
+        </div>
+        <div className='item'>
+          <Link to='/statistics'>
+            <button>
+              Statistics
+            </button>
+          </Link>
+        </div>
+        <div className='item'>
+          <Link to='/settings'>
+            <button>
+              Settings
+            </button>
+          </Link>
+        </div>
+        <div className='item'>
+          <button 
+            onClick={signOutUser}>
+            Sign Out
+          </button>
         </div>
       </div>
-    )
-  } else if (page === "create") {
-    return (
-      <CreateGamePage />
-    )
-  } else if (page === "join") {
-    return (
-      <JoinGamePage />
-    )
-  } else if (page === "store") {
-    return (
-      <StorePage />
-    )
-  } else if (page === "statistics") {
-    return (
-     <StatisticsPage />
-    )
-  } else if (page === "settings") {
-    return (
-      <SettingsPage />
-    )
-  } else {
-    return (
-      <div className='app'>
-        <h1>Error</h1>
-      </div>
-    )
-  }
-}
-
-
-
-
-
+    </div>
+  );
+};
 
